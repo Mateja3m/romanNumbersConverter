31d083cf25e87252f7dec78051bf8fc2fe8a7f34
Milan <milanmatejicdev@gmail.comn>
2021-10-31 21:05:57 +0100

    improved responsiveness

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 const arabicInput = document.getElementById("arabicNum");
 const romanInput = document.getElementById("romanNum");
 
-
-arabicInput.addEventListener('mousemove', function changeColor (e) {
-    document.body.style.background = "rgb("+e.offsetX+","+e.offsetY+",10)";
-})
-
-
-
+arabicInput.addEventListener("mousemove", function changeColor(e) {
+  document.body.style.background =
+    "rgb(" + e.offsetX + "," + e.offsetY + ",10)";
+});
 
 arabicInput.addEventListener("input", (e) => {
-    romanNum.value = convertToRoman(e.target.value);
+  romanNum.value = convertToRoman(e.target.value);
 });
 
 function convertToRoman(num) {
-    let romanNumbers = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
-    };
-    let roman = '';
-    for (let i in romanNumbers) {
-        while (num >= romanNumbers[i]) {
-            roman += i;
-            num -= romanNumbers[i];
-        }
+  let romanNumbers = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+  for (let i in romanNumbers) {
+    while (num >= romanNumbers[i]) {
+      roman += i;
+      num -= romanNumbers[i];
     }
-    return roman;
+  }
+  return roman;
 }
